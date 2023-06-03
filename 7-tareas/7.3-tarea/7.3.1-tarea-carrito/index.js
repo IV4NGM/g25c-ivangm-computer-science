35c0c742dc6bbff972bf0fbfc1672be3a596f7b6
IvanGM <ivangm_01@hotmail.com>
2023-06-03 00:55:15 -0600

    feat: Función borrar elementos añadida

--- 7-tareas/7.3-tarea/7.3.1-tarea-carrito/index.js
+++ 7-tareas/7.3-tarea/7.3.1-tarea-carrito/index.js
@@ -1,12 +1,34 @@
 class Articulo{
-    constructor(nombre, cantidad, rapida){
+    constructor(nombre, cantidad, rapida, id){
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.entrega_rapida = rapida;
+        this.id = id;
     }
 }
 
-function addElement(nombre, cantidad, rapida){
+let items = [];
+let id = 0;
+
+document.querySelector("#add_button").addEventListener("click", function(){
+    let nombre = document.querySelector("#name").value;
+    let cantidad = parseInt(document.querySelector("#amount").value);
+    let rapida = document.querySelector("#fast").checked;
+    if(nombre.trim()!="" && cantidad > 0){
+        id++;
+        items.push(new Articulo(nombre, cantidad, rapida, id));
+        alert("Elemento añadido correctamente.");
+        addElement(nombre, cantidad, rapida, id);
+        console.log(items);
+    }else{
+        alert("Debes llenar todos los campos correctamente.");
+    }
+});
+
+let itemsNumber = 0;
+
+function addElement(nombre, cantidad, rapida, id){
+    itemsNumber++;
     document.querySelector("#empty-car").style.display = "none";
     let generalContainer = document.createElement("div");
     generalContainer.classList.add("mdl-card", "mdl-shadow--2dp", "item-card");
@@ -31,24 +53,24 @@ function addElement(nombre, cantidad, rapida){
     if(rapida){
         tipoEntrega.style.color = "green";
     }
+    let botonCerrar = document.createElement("button");
+    botonCerrar.classList.add("mdl-button", "mdl-js-button", "mdl-button--fab", "mdl-button--colored", "remove");
+    let icono = document.createElement("i");
+    icono.classList.add("material-icons");
+    icono.innerHTML = "close";
+    botonCerrar.appendChild(icono);
     entregaContainer.appendChild(textEntrega);
     entregaContainer.appendChild(tipoEntrega);
     generalContainer.appendChild(entregaContainer);
+    generalContainer.appendChild(botonCerrar);
     document.querySelector("#cards-container").appendChild(generalContainer);
-}
-
-let items = [];
-
-document.querySelector("#add_button").addEventListener("click", function(){
-    let nombre = document.querySelector("#name").value;
-    let cantidad = parseInt(document.querySelector("#amount").value);
-    let rapida = document.querySelector("#fast").checked;
-    if(nombre.trim()!="" && cantidad > 0){
-        items.push(new Articulo(nombre, cantidad, rapida));
-        alert("Elemento añadido correctamente.");
-        addElement(nombre, cantidad, rapida);
+    botonCerrar.addEventListener("click", function(){
+        generalContainer.remove();
+        itemsNumber--;
+        if(itemsNumber <=0){
+            document.querySelector("#empty-car").style.display = "block";
+        }
+        items = items.filter(e => e.id !=id);
         console.log(items);
-    }else{
-        alert("Debes llenar todos los campos correctamente.");
-    }
-});
+    });
+}
