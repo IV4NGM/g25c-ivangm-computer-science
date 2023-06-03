c9fa6018b2ba6fc438753a8f47cfa0e2714272cd
IvanGM <ivangm_01@hotmail.com>
2023-06-02 23:45:01 -0600

    feat: Función de mostrar carrito en la pantalla añadida

--- 7-tareas/7.3-tarea/7.3.1-tarea-carrito/index.js
+++ 7-tareas/7.3-tarea/7.3.1-tarea-carrito/index.js
@@ -6,6 +6,37 @@ class Articulo{
     }
 }
 
+function addElement(nombre, cantidad, rapida){
+    document.querySelector("#empty-car").style.display = "none";
+    let generalContainer = document.createElement("div");
+    generalContainer.classList.add("mdl-card", "mdl-shadow--2dp", "item-card");
+    let productoContainer = document.createElement("div");
+    productoContainer.classList.add("row-container");
+    let image = document.createElement("img");
+    image.classList.add("carrito");
+    let productName = document.createElement("p");
+    productName.textContent = nombre;
+    productoContainer.appendChild(image);
+    productoContainer.appendChild(productName);
+    generalContainer.appendChild(productoContainer);
+    let cantParrafo = document.createElement("p");
+    cantParrafo.textContent = "Cantidad: " + cantidad.toString();
+    generalContainer.appendChild(cantParrafo);
+    let entregaContainer = document.createElement("div");
+    entregaContainer.classList.add("row-container", "row-container-entrega");
+    let textEntrega = document.createElement("p");
+    textEntrega.textContent = "Entrega:";
+    let tipoEntrega = document.createElement("p");
+    tipoEntrega.textContent = rapida ? "Rápida" : "Normal";
+    if(rapida){
+        tipoEntrega.style.color = "green";
+    }
+    entregaContainer.appendChild(textEntrega);
+    entregaContainer.appendChild(tipoEntrega);
+    generalContainer.appendChild(entregaContainer);
+    document.querySelector("#cards-container").appendChild(generalContainer);
+}
+
 let items = [];
 
 document.querySelector("#add_button").addEventListener("click", function(){
@@ -14,7 +45,8 @@ document.querySelector("#add_button").addEventListener("click", function(){
     let rapida = document.querySelector("#fast").checked;
     if(nombre.trim()!="" && cantidad > 0){
         items.push(new Articulo(nombre, cantidad, rapida));
-        alert("Elemento añadido correctamente.")
+        alert("Elemento añadido correctamente.");
+        addElement(nombre, cantidad, rapida);
         console.log(items);
     }else{
         alert("Debes llenar todos los campos correctamente.");
